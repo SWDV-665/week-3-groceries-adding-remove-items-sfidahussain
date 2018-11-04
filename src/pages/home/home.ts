a5398eac42bfaeb7f46e46438de27c60affbbc0d
sfidahussain <6472383+sfidahussain@users.noreply.github.com>
2018-11-04 15:37:34 -0600

    initial commit 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery List";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 1
+    },
+    {
+      name: "Bread",
+      quantity: 2
+    },
+    {
+      name: "Eggs",
+      quantity: 3
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public ToastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item, index) {
+    console.log("Removing item - ", item, index);
+    const toast = this.ToastCtrl.create({
+      message: "Removing item - " + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1)
+  }
+
+
+  addItem() {
+    console.log("Adding item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter an item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [ 
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked', data);
+          }
+        },
+        {
+          text: 'Save',
+          handler: item=> {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
